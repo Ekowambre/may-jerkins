b4c88a64a998d010a5408bfbbe2e0fcfe326ee97
Ekowambre <nyambekyere1@gmail.com>
2023-08-01 15:09:30 -0400

    first commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "ca-central-1"
+}
+
+
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
